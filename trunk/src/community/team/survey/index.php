28d1e9f67e11c16c7240600b1c54795de172a4e2
Raul Jimenez Ortega <raul@georemindme.com>
2011-06-11 15:20:33 +0200

    Adding licensing, css file, new icons and survey section. Also modified existing files

---
+++ trunk/src/community/team/survey/index.php
@@ -0,0 +1,21 @@
+<?php
+require '../../h2o/h2o.php';
+require '../../templates/menu_sections.php';
+h2o::load('i18n');
+
+$var_template='survey.html';
+$var_title='Community Survey';
+//None in this case
+#$active_section= 'Community'; //Values= Names on ../templates/menu_sections.php
+
+$template = new H2o('../../templates/'.$var_template, array(
+    //'cache_dir' => dirname(__FILE__),
+));
+
+echo $template->render(array(
+	'title'=> $var_title,
+	'menu_items'=>$menu_sections,
+	#'active_section'=>$active_section,
+	)
+);
+
